5c3c0be41b957fc269504949abd5d9516222422d
nss <116879643+nsaintsever@users.noreply.github.com>
2025-04-10 10:19:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ else:
 
 # Load data
 df = pd.read_csv("lacoste_sub_df.csv", sep=',', quotechar='"')
-df['date'] = pd.to_datetime(df['date'], format='%d/%m/%Y')
+df['date'] = pd.to_datetime(df['date'], format='ISO8601')
 df['millesime'] = pd.to_numeric(df['millesime']).astype(int)
 df['quantite_totale'] = df['per'] * df['quantite']
 
